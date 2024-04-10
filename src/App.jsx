5b8d9287cc4fafdfa479f63ab1036f2693cf3f3c
grgttdln <georgettedalen@gmail.com>
2024-04-10 16:45:35 +0800

    Delete data to DB Firebase through ID

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 import "./App.css";
 
 function App() {
   const [books, setBooks] = useState([]);
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
+  const [bookID, setBookID] = useState("");
 
   useEffect(() => {
     const firebaseConfig = {
@@ -62,6 +70,27 @@ function App() {
     setAuthor("");
   };
 
+  const handleDeleteBook = (event) => {
+    event.preventDefault();
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyBe7NJHkJAWBwkrFRCaqy4kPi6fL1NPoXk",
+      authDomain: "learnfirebase-82d14.firebaseapp.com",
+      projectId: "learnfirebase-82d14",
+      storageBucket: "learnfirebase-82d14.appspot.com",
+      messagingSenderId: "491316288982",
+      appId: "1:491316288982:web:a355ef4411c9b31fcb9877",
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
+    const docRef = doc(db, "books", bookID);
+    deleteDoc(docRef);
+
+    setBookID("");
+  };
+
   return (
     <>
       <div>
@@ -95,6 +124,22 @@ function App() {
             />
           </div>
 
+          <input type="submit" />
+        </form>
+      </div>
+
+      <div>
+        <h1>Delete a Book</h1>
+
+        <form onSubmit={handleDeleteBook}>
+          <div>
+            <label>book id: </label>
+            <input
+              type="text"
+              value={bookID}
+              onChange={(e) => setBookID(e.target.value)}
+            />
+          </div>
           <input type="submit" />
         </form>
       </div>
